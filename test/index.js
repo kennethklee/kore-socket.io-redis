cf76f755ced3dce330308d9391c5f72e46e874f0
kennethklee <kennethkl@gmail.com>
2014-02-04 22:34:22 -0500

    tests not passing, perhaps redis-mock is not up to spec

--- test/index.js
+++ test/index.js
@@ -1,6 +1,34 @@
+var redis = require('redis-mock'),
+    client = redis.createClient(),
+    RedisAdapter = require('../lib');
+
+require('should');
+
 describe('Redis Adapter', function() {
-    it('should add socket to room');
+
+    it('should add socket to room', function(done) {
+        var expectedId = 'test1',
+            expectedRoom = 'test1',
+            adapter = new RedisAdapter({
+                pub: client,
+                sub: client,
+                cmd: client
+            });
+
+        adapter.add(expectedId, expectedRoom, function() {
+            client.keys('rooms:' + expectedRoom + ':sockets', function(err, keys) {
+                keys.length.should.equal(1);
+
+                client.keys('sockets:' + expectedId + ':rooms', function(err, keys) {
+                    keys.length.should.equal(1);
+
+                    done();
+                });
+            });
+        });
+    });
     it('should remove socket from room');
     it('should remove socket from all rooms');
     it('should send packet to all sockets in rooms');
+
 });
