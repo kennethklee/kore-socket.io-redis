9af329ab65c16878a58c2d8ae64180688f02077b
Kenneth Lee <kennethkl@gmail.com>
2014-02-05 03:20:43 +0000

    initial redis adapter

---
+++ test/index.js
@@ -0,0 +1,6 @@
+describe('Redis Adapter', function() {
+    it('should add socket to room');
+    it('should remove socket from room');
+    it('should remove socket from all rooms');
+    it('should send packet to all sockets in rooms');
+});
