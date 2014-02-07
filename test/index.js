ed764bb8af5eb9c98feda0fdb420f1a95c756421
kennethklee <kennethkl@gmail.com>
2014-02-06 23:06:14 -0500

    completed 2 tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,24 @@
-var redis = require('redis-mock'),
-    client = redis.createClient(),
+var redis = require('node-redis-mock'),
     RedisAdapter = require('../lib');
 
 require('should');
 
 describe('Redis Adapter', function() {
+    var adapter, client;
+
+    before(function(done) {
+        client = redis.createClient();
+        adapter = new RedisAdapter(null, {
+            pub: client,
+            sub: client,
+            cmd: client
+        });
+        done();
+    });
 
     it('should add socket to room', function(done) {
         var expectedId = 'test1',
-            expectedRoom = 'test1',
-            adapter = new RedisAdapter({
-                pub: client,
-                sub: client,
-                cmd: client
-            });
+            expectedRoom = 'test1';
 
         adapter.add(expectedId, expectedRoom, function() {
             client.keys('rooms:' + expectedRoom + ':sockets', function(err, keys) {
@@ -27,8 +32,65 @@ describe('Redis Adapter', function() {
             });
         });
     });
-    it('should remove socket from room');
-    it('should remove socket from all rooms');
-    it('should send packet to all sockets in rooms');
+
+    it('should remove socket from room', function(done) {
+        var expectedId = 'test1',
+            expectedRoom = 'test1';
+
+        adapter.del(expectedId, expectedRoom, function() {
+            client.keys('rooms:' + expectedRoom + ':sockets', function(err, keys) {
+                keys.length.should.equal(0);
+
+                client.smembers('sockets:' + expectedId + ':rooms', function(err, rooms) {
+                    rooms.length.should.equal(0);
+
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should remove socket from all rooms', function(done) {
+        var expectedId = 'test1';
+        adapter.add(expectedId, 'room1');
+        adapter.add(expectedId, 'room2');
+
+        setTimeout(function() {
+            adapter.delAll(expectedId, function() {
+                done();
+            });
+        }, 100);
+    });
+
+
+    it.skip('should send packet to all sockets in rooms', function(done) {
+        var packetCount = 0,
+            socket1 = 'socket1',
+            socket2 = 'socket2',
+            room = 'room1',
+            mockedSocket = {
+                packet: function() {
+                    packetCount++;
+
+                    if (packetCount === 2) {
+                        done();
+                    }
+                }
+            };
+
+        adapter.nsp = {
+            connected: {
+                socket1: mockedSocket,
+                socket2: mockedSocket
+            }
+        };
+
+        adapter.add(socket1, room);
+        adapter.add(socket2, room);
+
+        setTimeout(function() {
+            adapter.broadcast('packet', {rooms: [room]});
+        }, 100);
+    });
 
 });
